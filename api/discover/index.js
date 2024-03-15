c1f053aac7cc2762c98b8ea5c6250d112d4b3e3b
dongguaguaguagua <1448424184@qq.com>
2024-03-15 15:18:54 +0800

    support url parse

--- api/discover/index.js
+++ api/discover/index.js
@@ -1,22 +1,25 @@
 const axios = require('axios');
+const url = require('url');
+const querystring = require('querystring');
+
 const apiKey = process.env.TMDB_API_KEY;
 // const apiKey = process.env.TMDB_API_KEY;
 
 module.exports = async (req, res) => {
-  const { body } = req;
-const tmdbUrl = `https://api.themoviedb.org/3/discover/${body.type}\
-?include_adult=${body.include_adult}\
-&language=${body.language}\
-&page=${body.page}\
-&sort_by=${body.sort_by}\
+  const { url: requestUrl, body } = req;
+  const parsedUrl = url.parse(requestUrl);
+  const query = querystring.parse(parsedUrl.query);
+
+  const tmdbUrl = `https://api.themoviedb.org/3/discover/${body.type}\
+?include_adult=${query.include_adult || body.include_adult}\
+&language=${query.language || body.language}\
+&page=${query.page || body.page}\
+&sort_by=${query.sort_by || body.sort_by}\
 &api_key=${apiKey}`;
   try {
-    // 发送 HTTP 请求以获取 TMDb API 的响应
     const response = await axios.get(tmdbUrl);
-    // 将 TMDb API 的响应返回给调用方
     res.send(response.data);
   } catch (error) {
-    // 处理错误情况
-    res.status(500).send(`${tmdbUrl}`);
+    res.status(500).send('Error fetching data from TMDb API');
   }
 };
